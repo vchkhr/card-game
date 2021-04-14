59c85eff61ab3885a45183f4f82225ecef816caa
Viacheslav Kharchenko <vkharchenk@c1r7p3.khpi.ucode-connect.study>
2021-04-14 16:43:15 +0300

    merge

--- view/index.php
+++ view/index.php
@@ -1,12 +1,10 @@
 <?php
 require_once("NavigationManager.php");
-require_once("../controller/ControllerGame.php");
 require_once("../controller/ControllerRegister.php");
 require_once("../model/Model.php");
 require_once("../model/User.php");
 require_once("../model/UserDB.php");
 require_once("../model/DatabaseConnection.php");
-require_once("../model/data_call/Player.php");
 
 $register = "./res/html/registration.html";
 $login = "./res/html/login.html";
@@ -23,53 +21,30 @@ $manager->putScreen('adminCard', $adminCard);
 $manager->putScreen('userCard', $userCard);
 $manager->putScreen('game', $game);
 
-// print_r($manager->arrFragment);
+
+//print_r($manager->arrFragment);
 $screenController = new ControllerRegister($manager);
 $manager->renderBy('register');
 
 if (isset($_POST['registerUser'])) {
     if ($_POST['registerUser']['password'] == $_POST['registerUser']['confirmPassword']) {
         $screenController->register(arrayToUser($_POST['registerUser']));
-    }
-    else {
+    }else{
         notify('Incorrect confirm password');
     }
-}
-elseif (isset($_POST['loginUser'])) {
+} elseif (isset($_POST['loginUser'])) {
     $screenController->login($_POST['loginUser']['login'], $_POST['loginUser']['password']);
-}
-elseif (isset($_POST['forgetLogin'])) {
+} elseif (isset($_POST['forgetLogin'])) {
     $screenController->forget($_POST['forgetLogin']);
-}
-elseif (isset($_POST['forger'])) {
+} elseif (isset($_POST['forger'])) {
     $manager->renderBy('forget');
-}
-elseif (isset($_POST['register'])) {
+} elseif (isset($_POST['register'])) {
     $manager->renderBy('register');
-}
-elseif (isset($_POST['login'])) {
+} elseif (isset($_POST['login'])) {
     $manager->renderBy('login');
-}
-elseif (isset($_POST['userCard'])) {
-    $manager->renderBy('userCard');
-}
-elseif (isset($_POST['game'])) {
+} elseif (isset($_POST['game'])) {
     $manager->changeScreen('game');
-
-    $player1 = new Player(30, "pl1");
-    $player2 = new Player(30, "pl2");
-
-    $game = new ControllerGame(player1, player2);
-
-    $manager->bind("#PLAYER_1_NAME#", $player1->name);
-    $manager->bind("#PLAYER_2_NAME#", $player2->name);
-
-    $manager->bind("#PLAYER_1_IMG#", $player1->imgUrl);
-    $manager->bind("#PLAYER_2_IMG#", $player2->imgUrl);
-
-    $manager->bind("#PLAYER_1_HEALTH#", $player1->hp);
-    $manager->bind("#PLAYER_2_HEALTH#", $player2->hp);
-
+    $manager->bind("#USER_NAME#", $userDb->login);
     $manager->render();
 }
 
