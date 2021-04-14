e3cc25c6a819679179a998acc72ec4be5ec5211d
Viacheslav Kharchenko <vchkhr@gmail.com>
2021-04-14 17:00:46 +0300

    Fix db location

--- view/index.php
+++ view/index.php
@@ -1,10 +1,10 @@
 <?php
 require_once("NavigationManager.php");
 require_once("../controller/ControllerRegister.php");
-require_once("../model/Model.php");
-require_once("../model/User.php");
-require_once("../model/UserDB.php");
-require_once("../model/DatabaseConnection.php");
+require_once("../model/db/Model.php");
+require_once("../model/db/User.php");
+require_once("../model/db/UserDB.php");
+require_once("../model/db/DatabaseConnection.php");
 
 $register = "./res/html/registration.html";
 $login = "./res/html/login.html";
